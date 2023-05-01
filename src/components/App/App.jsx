93c839ebb0dbf523753675224c1c3c88ce976a2b
“Ildar <ildarabyi.92@gmail.com>
2023-05-01 20:19:03 +0300

    commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,11 +16,8 @@ export default class App extends React.Component {
   editTask = (id) => {
     const { todoData } = this.state;
     const idx = todoData.findIndex((el) => el.id === id);
-    console.log(idx);
     const oldItem = todoData[idx];
-    console.log(oldItem);
     const newItem = { ...oldItem, edit: !oldItem.edit };
-    console.log(newItem);
     const newArray = [...todoData.slice(0, idx), newItem, ...todoData.slice(idx + 1)];
     this.setState(() => {
       return {
@@ -106,16 +103,12 @@ export default class App extends React.Component {
   };
   changeTimerValue = (id, value) => {
     this.setState(({ todoData }) => {
-      console.log(todoData);
       const index = todoData.findIndex((el) => {
         return el.id === id;
       });
-      console.log(index);
       const oldItem = todoData[index];
-      console.log(oldItem);
       if (typeof oldItem === 'undefined') return;
       const newItem = { ...oldItem, timer: value };
-      console.log(newItem);
       const newArray = [...todoData.slice(0, index), newItem, ...todoData.slice(index + 1)];
 
       return {
@@ -149,134 +142,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-// /* eslint-disable indent */
-// import React from 'react';
-
-// import NewTaskForm from '../NewTaskForm/NewTaskForm';
-// import Footer from '../Footer/Footer';
-// import TaskList from '../TaskList/TaskList';
-
-// import './App.css';
-
-// export default class App extends React.Component {
-//   maxId = 1;
-//   state = {
-//     todoData: [],
-//     filter: 'All',
-//   };
-
-//   addItem = (text) => {
-//     const newItem = {
-//       id: this.maxId++,
-//       task: text,
-//       completed: false,
-//       date: new Date(),
-//       edit: false,
-//     };
-
-//     this.setState(({ todoData }) => {
-//       const newData = [...todoData, newItem];
-//       return {
-//         todoData: newData,
-//       };
-//     });
-//   };
-
-//   deleteItem = (id) => {
-//     this.setState(({ todoData }) => {
-//       const newArray = todoData.filter((item) => item.id !== id);
-//       return {
-//         todoData: newArray,
-//       };
-//     });
-//   };
-
-//   onToggleCompleted = (id) => {
-//     this.setState(({ todoData }) => {
-//       const idx = todoData.findIndex((el) => el.id === id);
-//       const oldItem = todoData[idx];
-//       const newItem = { ...oldItem, completed: !oldItem.completed };
-//       const newArray = [...todoData.slice(0, idx), newItem, ...todoData.slice(idx + 1)];
-
-//       return {
-//         todoData: newArray,
-//       };
-//     });
-//   };
-
-//   filterTask = () => {
-//     const { todoData, filter } = this.state;
-
-//     if (filter === 'All') return todoData;
-//     return todoData.filter((item) => (filter === 'Completed' ? item.completed : !item.completed));
-//   };
-
-//   onFilterChange = (filter) => {
-//     this.setState({ filter });
-//   };
-
-//   clearCompleted = () => {
-//     this.setState(({ todoData }) => {
-//       const newData = todoData.filter((item) => !item.completed);
-//       return {
-//         todoData: newData,
-//       };
-//     });
-//   };
-
-//   editTask = (id) => {
-//     const { todoData } = this.state;
-//     const idx = todoData.findIndex((el) => el.id === id);
-//     const oldItem = todoData[idx];
-//     const newItem = { ...oldItem, edit: !oldItem.edit };
-//     const newArray = [...todoData.slice(0, idx), newItem, ...todoData.slice(idx + 1)];
-//     this.setState(() => {
-//       return {
-//         todoData: newArray,
-//       };
-//     });
-//   };
-
-//   onSubmitEdit = (event, id) => {
-//     event.preventDefault();
-//     this.setState(({ todoData }) => {
-//       const index = todoData.findIndex((data) => data.id === id);
-//       const oldData = todoData[index];
-//       const newData = {
-//         ...oldData,
-//         edit: !oldData.edit,
-
-//         task: event.target[0].value,
-//       };
-//       const newArray = [...todoData.slice(0, index), newData, ...todoData.slice(index + 1)];
-//       return {
-//         todoData: newArray,
-//       };
-//     });
-//   };
-
-//   render() {
-//     const completedCount = this.state.todoData.filter((el) => !el.completed).length;
-//     return (
-//       <section className="todoapp">
-//         <NewTaskForm addItem={this.addItem} />
-//         <section className="main">
-//           <TaskList
-//             todoData={this.filterTask()}
-//             deleteItem={this.deleteItem}
-//             onToggleCompleted={this.onToggleCompleted}
-//             editTask={this.editTask}
-//             onSubmitEdit={this.onSubmitEdit}
-//           />
-
-//           <Footer
-//             completedCount={completedCount}
-//             onFilterChange={this.onFilterChange}
-//             clearCompleted={this.clearCompleted}
-//           />
-//         </section>
-//       </section>
-//     );
-//   }
-// }
